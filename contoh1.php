1345050de59ea7f43391d6f1fcc788c375a1650e
sinomrr <109023196+sinomrr@users.noreply.github.com>
2022-09-20 14:34:02 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<hi>Perkenalkan</h1>";
+        echo "Nama saya Citra Dwi Maharani
+            Saya tinggal di daerah bekasi
+            Olaharaga yang saya sukai adalah Berenang";
+    }
+}
