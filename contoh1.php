8e107dbe1182902ac73dad473334330591b1dde6
sinomrr <109023196+sinomrr@users.noreply.github.com>
2022-09-27 11:21:44 +0700

    Update contoh1.php

--- contoh1.php
+++ contoh1.php
@@ -4,8 +4,8 @@ class contoh1 extends CI_Controller
     public function index()
     {
         echo "<hi>Perkenalkan</h1>";
-        echo "Nama saya Citra Dwi Maharani
+        echo "Nama saya Sinom Rahardjo
             Saya tinggal di daerah bekasi
-            Olaharaga yang saya sukai adalah Berenang";
+            Olaharaga yang saya sukai adalah Ngegame";
     }
 }
